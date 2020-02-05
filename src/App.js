aa5b33053d05bba722cd2091aff42ab51e450742
Olakunle Boye <kunlexzy@gmail.com>
2020-02-05 08:19:14 +0100

    project initialize

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './bootstrap.min.css';
+
+class App extends React.Component {
+  render(){
+    return (
+    <div className="container">
+      <div className="row">
+      it working
+      </div>
+    </div>
+  )
+  }
+}
+
+export default App;
