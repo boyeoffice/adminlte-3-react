40bcb5c4bde982067658438625e8de73a01f777c
Olakunle Boye <kunlexzy@gmail.com>
2020-02-10 22:55:22 +0100

    scalfold some pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import './bootstrap.min.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
+//import './bootstrap.min.css';
+import AuthPage from './pages/Auth';
+import BookingsPage from './pages/Bookings';
+import EventsPage from './pages/Events';
 
-class App extends React.Component {
+class App extends Component {
   render(){
     return (
-    <div className="container">
-      <div className="row">
-      it working
-      </div>
-    </div>
-  )
+    <BrowserRouter>
+      <Switch>
+        <Redirect from="/" to="/auth" exact />
+        <Route path="/auth" component={AuthPage} />
+        <Route path="/events" component={EventsPage} />
+        <Route path="/bookings" component={BookingsPage} />
+      </Switch>
+    </BrowserRouter>
+  );
   }
 }
 
